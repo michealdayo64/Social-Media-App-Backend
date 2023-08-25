1f120a4b21eb3619f31e0a289ef12b2dd980b221
michealdayo64 <omotoshomicheal93@gmail.com>
2023-08-25 07:13:38 +0100

    ok

--- social_groups/consumers.py
+++ social_groups/consumers.py
@@ -27,4 +27,21 @@ class GroupConsumer(AsyncJsonWebsocketConsumer):
         Called when the websockect closes for any reason
         """
         print("PublicChatConsumer: diconnect")
+
+
+    async def chat_message(self, event):
+        """
+        Called when someone has messaged our chat
+        """
+        # Send a message down to the client
+        print("PublicChatConsumer: chat_message from user #: " + str(event['user_id']))
+        #timestamp = calculate_timestamp(timezone.now())
+        await self.send_json({
+            #"msg_type": MSG_TYPE_MESSAGE,
+            #"profile_image": event['profile_image'],
+            "username": event['username'],
+            'user_id': event['user_id'],
+            'message': event['message'],
+            #"natural_timestamp": timestamp
+        })
         
