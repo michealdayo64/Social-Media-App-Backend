35e6d7967929819c66899c562d90c6b2d3558d24
michealdayo64 <omotoshomicheal93@gmail.com>
2023-09-16 09:46:38 +0100

    pk

--- friend_app/admin.py
+++ friend_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import FriendsList
 # Register your models here.
+
+admin.site.register(FriendsList)
--- friend_app/models.py
+++ friend_app/models.py
@@ -1,3 +1,49 @@
 from django.db import models
-
+from account.models import Account
 # Create your models here.
+
+
+class FriendsList(models.Model):
+    user = models.ForeignKey(
+        Account, on_delete=models.CASCADE, null=True, blank=True, related_name="user")
+    friends = models.ManyToManyField(
+        Account, null=True, blank=True, related_name="friends")
+
+    def __str__(self):
+        return self.user.username
+
+    def add_friend(self, account):
+        """
+        Add a new friend
+        """
+        if not account in self.friends.all():
+            self.friends.add(account)
+            self.save()
+
+    def remove_friend(self, account):
+        """
+        Remove a friend
+        """
+        if account in self.friends.all():
+            self.friends.remove(account)
+
+    def unfriend(self, removee):
+        """
+        Initiate the action of unfriending someone
+        """
+        remover_friends_list = self  # person terminating the frienship
+
+        # Remove friend from romover friends list
+        remover_friends_list.remove_friend(removee)
+
+        # Remove friend from removee friends list
+        friends_list = FriendsList.objects.get(user=removee)
+        friends_list.remove_friend(self.user)
+
+    def is_mutual_friend(self, friend):
+        """
+        Is this a friend
+        """
+        if friend in self.friends.all():
+            return True
+        return False
