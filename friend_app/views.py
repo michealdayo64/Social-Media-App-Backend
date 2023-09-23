821df53d1a7fc172a81c15e1473c3297bb53f327
michealdayo64 <omotoshomicheal93@gmail.com>
2023-09-23 06:48:58 +0100

    ok

--- friend_app/views.py
+++ friend_app/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from friend_app.models import FriendsList, FriendRequest
 from account.models import Accounts
+from .friendRequestStatus import FriendRequestStatus
+from .utils import get_friend_request_or_false
 
 # Create your views here.
 
@@ -18,11 +20,49 @@ def friend_index(request):
         all_user = Accounts.objects.exclude(id=user_id)
 
         accounts = []
-        auth_user_friend_list = FriendsList.objects.get(user=user)
+        """is_self = True
+        is_friend = False
+        request_sent = FriendRequestStatus.NO_REQUEST_SENT.value
+        friend_requests = None"""
+        
         for account in all_user:
-            print(auth_user_friend_list.friends.all().count())
+            try:
+                auth_user_friend_list = FriendsList.objects.get(user=account)
+            except FriendsList.DoesNotExist:
+                auth_user_friend_list = FriendsList(user = account)
+                auth_user_friend_list.save()
+
+            is_friend = False
+            request_sent = FriendRequestStatus.NO_REQUEST_SENT.value
+            friend_requests = None
+
+            friends = auth_user_friend_list.friends.all()
+            if friends.filter(pk = user.id):
+                is_friend = True
+            else:
+                is_friend = False
+
+                #CASE1: Request has been sent from THEM to YOU:
+                #FriendRequestStatus.THEM_SENT_TO_YOU
+                if get_friend_request_or_false(sender = account, reciever = user) != False:
+                    request_sent = FriendRequestStatus.THEM_SENT_TO_YOU.value
+
+                #CASE1: Request has been sent from YOU to THEM:
+                #FriendRequestStatus.YOU_SENT_TO_THEM
+                elif get_friend_request_or_false(sender = user, reciever = account) != False:
+                    request_sent = FriendRequestStatus.YOU_SENT_TO_THEM.value
+
+                #CASE1: No Request has been sent. FriendRequestStatus.NO_REQUEST_SENT
+                else:
+                    request_sent = FriendRequestStatus.NO_REQUEST_SENT.value
+            try:
+                friend_requests = FriendRequest.objects.filter(reciever = user, is_active = True)
+            except:
+                pass
+            
+            print(is_friend, request_sent, friend_requests)
             accounts.append(
-                (account, auth_user_friend_list.is_mutual_friend(account), auth_user_friend_list.friends.all().count()))
+                (account, auth_user_friend_list.is_mutual_friend(account), auth_user_friend_list.friends.all().count(), is_friend, request_sent, friend_requests))
         print(accounts)
         context = {
             'accounts': accounts
