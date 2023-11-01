fa23cbb7125bb3b6293bb6288ce113081025898c
michealdayo64 <omotoshomicheal93@gmail.com>
2023-11-01 03:38:56 +0100

    ok

--- message_app/models.py
+++ message_app/models.py
@@ -121,3 +121,61 @@ class UnreadChatRoomMessages(models.Model):
             return self.room.user2
         else:
             return self.room.user1
+        
+@receiver(post_save, sender=PrivateChatRoom)
+def create_unread_chatroom_messages_obj(sender, instance, created, **kwargs):
+	if created:
+		unread_msgs1 = UnreadChatRoomMessages(room=instance, user=instance.user1)
+		unread_msgs1.save()
+
+		unread_msgs2 = UnreadChatRoomMessages(room=instance, user=instance.user2)
+		unread_msgs2.save()
+
+@receiver(pre_save, sender=UnreadChatRoomMessages)
+def increment_unread_msg_count(sender, instance, **kwargs):
+    """
+    When the unread message count increases, update the notification. 
+    If one does not exist, create one. (This should never happen)
+    """
+    if instance.id is None: # new object will be created
+        pass # create_unread_chatroom_messages_obj will handle this scenario
+    else:
+        previous = UnreadChatRoomMessages.objects.get(id=instance.id)
+        print(previous.count)
+        if previous.count < instance.count: # if count is incremented
+            content_type = ContentType.objects.get_for_model(instance)
+            if instance.user == instance.room.user1:
+                other_user = instance.room.user2
+            else:
+                other_user = instance.room.user1
+            try:
+                notification = Notification.objects.get(target=instance.user, content_type=content_type, object_id=instance.id)
+                notification.verb = instance.most_recent_message
+                notification.timestamp = timezone.now()
+                notification.save()
+            except Notification.DoesNotExist:
+                instance.notifications.create(
+					target=instance.user,
+					from_user=other_user,
+					redirect_url=f"{settings.BASE_URL}/chat/?room_id={instance.room.id}", # we want to go to the chatroom
+					verb=instance.most_recent_message,
+					content_type=content_type,
+				)
+                
+@receiver(pre_save, sender=UnreadChatRoomMessages)
+def remove_unread_msg_count_notification(sender, instance, **kwargs):
+	"""
+	If the unread messge count decreases, it means the user joined the chat. So delete the notification.
+	"""
+	if instance.id is None: # new object will be created
+		pass # create_unread_chatroom_messages_obj will handle this scenario
+	else:
+		previous = UnreadChatRoomMessages.objects.get(id=instance.id)
+		if previous.count > instance.count: # if count is decremented
+			content_type = ContentType.objects.get_for_model(instance)
+			try:
+				notification = Notification.objects.get(target=instance.user, content_type=content_type, object_id=instance.id)
+				notification.delete()
+			except Notification.DoesNotExist:
+				pass
+				# Do nothing
