0eb758cf99bf3b75e46835c8597d7450e6b1efe6
michealdayo64 <omotoshomicheal93@gmail.com>
2024-07-12 06:27:25 +0100

    ok

--- social_app/serializers.py
+++ social_app/serializers.py
@@ -4,23 +4,23 @@ from .models import Post, Comment
 from account.serializers import UserSerializer
 
 
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False)
+    # post_id = PostSerializer(many=False)
+
+    class Meta:
+        model = Comment
+        fields = ('pk', 'user', 'comment',
+                  'date_comment', 'date_comment_update', )
+
+
 class PostSerializer(serializers.ModelSerializer):
     user = UserSerializer(many=False)
     user_like_post = UserSerializer(many=True)
-    comments = serializers.StringRelatedField(many=True)
+    comments = CommentSerializer(many=True)
     repost_users = UserSerializer(many=True)
 
     class Meta:
         model = Post
         fields = ('pk', 'user', 'user_post', 'comments', 'file', 'date_post',
                   'date_post_update', 'user_like_post', 'reposted_by', 'repost_users')
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    user = UserSerializer(many=False)
-    post_id = PostSerializer(many=False)
-
-    class Meta:
-        model = Comment
-        fields = ('pk', 'user', 'post_id', 'comment',
-                  'date_comment', 'date_comment_update', )
